fb4aa2bcf0f4eb9dec1dfab5c30f2a4bc99f5142
Michael Woolnough <michael.woolnough@gmail.com>
2013-07-09 10:41:23 +0100

    Added tests

---
+++ limited_test.go
@@ -0,0 +1,30 @@
+package lw
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestLimitedWriter(t *testing.T) {
+	b := new(bytes.Buffer)
+	l := LimitWriter(b, 10)
+	n, err := l.Write([]byte{ 0, 1, 2, 3 })
+	if err != nil {
+		t.Fatalf("error received - %s", err)
+	} else if n != 4 {
+		t.Fatalf("expected 4 bytes written, got %d", n)
+	}
+	n, err = l.Write([]byte{ 4, 5, 6 })
+	if err != nil {
+		t.Fatalf("error received - %s", err)
+	} else if n != 3 {
+		t.Fatalf("expected 3 bytes written, got %d", n)
+	}
+	n, err = l.Write([]byte{ 7, 8, 9, 10, 11, 12 })
+	if _, ok := err.(*NoSpace); !ok {
+		t.Fatalf("expected error of type NoSpace, got %s", err)
+	} else if n != 3 {
+		t.Fatalf("expected 3 bytes written, got %d", n)
+	}
+	
+}
